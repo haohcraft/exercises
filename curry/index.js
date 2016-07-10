b1fa2c48208bc9ecd58dba5c94f3c5f91a1b6637
Hao Huang <haohcraft@gmail.com>
2016-07-09 22:16:05 -0400

    _.curry

---
+++ curry/index.js
@@ -0,0 +1,23 @@
+
+// key points: 
+// 1. [].slice.call(arguments)
+// 2. concat current arguments to existing ones
+// 
+var curry = function(fnc) {
+	var fncLeng = fnc.length;
+	var curried = function() {
+		var args = [].slice.call(arguments);
+		if(args.length === fncLeng) {
+			return fnc.apply(null, args);
+		} else {
+			return function() {
+				var _args = [].slice.call(arguments);
+				return curried.apply(null, args.concat(_args));
+			};
+		}
+	};
+
+	return curried;
+};
+
+module.exports = curry;
