c73809e3d1be2a6af2ba468c296576edf02ab3b8
Hao Huang <hhuang@dataminr.com>
2016-08-16 23:52:14 -0400

    more solution on flatten

--- flatten/index.js
+++ flatten/index.js
@@ -1,4 +1,4 @@
-var flatten = function(arr) {
+var flatten1 = function(arr) {
     if(!arr) return [];
     if(arr && arr.length === 1) return arr;
     var result = [];
@@ -12,5 +12,28 @@ var flatten = function(arr) {
     }
     return result;
 };
+
+var flatten2 = function(arr) {
+    if(!arr || arr.length < 2) return arr || [];
+    return arr.reduce(function(r, v) {
+        if(v instanceof Array) {
+            r = r.concat(flatten(v));
+        } else {
+            r.push(v);
+        }
+        return r;
+    }, []);
+}
+
+var flatten = function(arr) {
+    if(!arr || !arr.length) return arr || [];
+    if(arr && arr.length === 1 && !arr[0] instanceof Array) {
+        return arr;
+    }
+    var head = arr[0];
+    var rest = arr.slice(1);
+
+    return [].concat(flatten(head)).concat(flatten(rest));
+}
 
 module.exports = flatten;
