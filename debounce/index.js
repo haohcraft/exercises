bcde8909b69a8f041bf62e8d9c13c67056d51c0e
Hao Huang <hao.huang@datadoghq.com>
2017-09-13 17:46:42 -0400

    debounce

--- debounce/index.js
+++ debounce/index.js
@@ -1,5 +1,16 @@
-var debounce = function(fn) {
-   
+var debounce = function(fn, wait) {
+    var timer;
+    return function() {
+        var self = this; // To keep the context
+        var args = [].slice.call(arguments);
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+            fn.apply(self, args);
+            clearTimeout(timer);
+        }, wait);
+    };
 }
 
 module.exports = debounce;
